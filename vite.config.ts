148bcb9a9c0a119a6854f0e1d0aec813cb5f7342
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2026-05-04 09:53:13 +0700

    Configure SvelteKit project

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,6 @@
-import tailwindcss from "@tailwindcss/vite";
-import { devtools } from "@tanstack/devtools-vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "url";
+import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import viteTsConfigPaths from "vite-tsconfig-paths";
 
-const config = defineConfig({
-	resolve: {
-		alias: {
-			"@": fileURLToPath(new URL("./src", import.meta.url)),
-		},
-	},
-	plugins: [
-		devtools(),
-		// this is the plugin that enables path aliases
-		viteTsConfigPaths({
-			projects: ["./tsconfig.json"],
-		}),
-		tailwindcss(),
-		tanstackStart(),
-		viteReact(),
-	],
+export default defineConfig({
+	plugins: [sveltekit()]
 });
-
-export default config;
